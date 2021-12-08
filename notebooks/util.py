7a93137333ce2ffd19f840bdecd7fffebea8a230
CGPinDC <crystaldgould@gmail.com>
2021-12-08 14:08:58 -0500

    Removed df from evaluation functon

--- notebooks/util.py
+++ notebooks/util.py
@@ -6,7 +6,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, \
 from sklearn.model_selection import cross_val_score
 
 #evaluation functions
-def model_evaluation(model, X_tr, y_tr, X_te, y_te, labels, df): 
+def model_evaluation(model, X_tr, y_tr, X_te, y_te, labels): 
     '''
     Evaluates model and returns scores cross-val-score, accuracy, precision, recall, f1, and cohen-kappa,  
     then plots a confusion matrix and ROC-AUC graph. 
@@ -48,7 +48,6 @@ def model_evaluation(model, X_tr, y_tr, X_te, y_te, labels, df):
     #store scores
     scores = [[model, avg_cros_val, accuracy, precision, recall, f1, cohen_kappa, roc_auc]]
     scores_df = pd.DataFrame(scores, columns=['model', 'cross_val', 'accuracy','precision','recall','f1','cohen-kappa', 'roc-auc'])
-    df = df.append(scores_df)
 
     # roc curve for classes
     fpr = {}
@@ -61,22 +60,22 @@ def model_evaluation(model, X_tr, y_tr, X_te, y_te, labels, df):
     #plot evaluation graphs
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16,6))
 
-    ConfusionMatrixDisplay.from_predictions(y_te, preds, display_labels=le.classes_, ax=ax1)
+    ConfusionMatrixDisplay.from_predictions(y_te, preds, display_labels=labels, ax=ax1)
     ax1.set_title('Confusion Matrix')
 
-    for i in range(len(le.classes_)):    
+    for i in range(classes):    
         fpr[i], tpr[i], thresh[i] = roc_curve(y_te, preds_proba[:,i], pos_label=i)
 
         #plot binary roc-auc
     if classes == 2: 
-        ax2.plot(fpr[0], tpr[0], linestyle='--',color='orange', label=str(le.inverse_transform([0])))
-        ax2.plot(fpr[1], tpr[1], linestyle='--',color='green', label=str(le.inverse_transform([1])))
+        ax2.plot(fpr[0], tpr[0], linestyle='--',color='orange', label=str('Conservative'))
+        ax2.plot(fpr[1], tpr[1], linestyle='--',color='green', label=str('Liberal'))
     
     #plot mulit-class roc
     else: 
-        ax2.plot(fpr[0], tpr[0], linestyle='--',color='orange', label=str(le.inverse_transform([0])))
-        ax2.plot(fpr[1], tpr[1], linestyle='--',color='green', label=str(le.inverse_transform([1])))
-        ax2.plot(fpr[2], tpr[2], linestyle='--',color='blue', label=str(le.inverse_transform([2])))
+        ax2.plot(fpr[0], tpr[0], linestyle='--',color='orange', label=str('Center'))
+        ax2.plot(fpr[1], tpr[1], linestyle='--',color='green', label=str('Conservative'))
+        ax2.plot(fpr[2], tpr[2], linestyle='--',color='blue', label=str('Liberal'))
 
     # plotting
     ax2.plot([0,1], [0,1], linestyle='--', color='grey')    
